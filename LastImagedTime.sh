e16ec48b8a69561fc36f19435cd13b4c4b92d3ca
Richard Purves <r.purves@arts.ac.uk>
2013-10-30 22:30:19 +0000

    Create LastImagedTime.sh

---
+++ LastImagedTime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to report when the computer was last imaged
+# This is reliant on a timestamp file created as part of the firstrun script.
+
+# Author	 	: r.purves@arts.ac.uk
+# Version 1.0	: 09-08-2013 - Initial Version
+
+if [ -z /usr/lastimaged ] ;
+then
+echo "<result>Not Available</result>"
+else
+echo "<result>`cat /usr/lastimaged`</result>"
+fi
