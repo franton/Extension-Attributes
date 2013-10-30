3b61dd28ce287261d8c88cfc715f40edafc13823
Richard Purves <r.purves@arts.ac.uk>
2013-10-30 22:31:28 +0000

    Create LastUpdateTime.sh

---
+++ LastUpdateTime.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script to report when the computer was last updated
+# This is reliant on a timestamp file created as part of the update process
+
+# Author	 	: r.purves@arts.ac.uk
+# Version 1.0	: 12-08-2013 - Initial Version
+
+if [ -z /usr/lastupdated ] ;
+then
+echo "<result>Not Available</result>"
+else
+echo "<result>`cat /usr/lastupdated`</result>"
+fi
