292b3faac6696c565a08132fb403ba091a3fb5e2
Richard Purves <r.p>
2014-02-12 21:53:36 +0000

    Create HDBootVolName
    This detects if the Mac’s boot volume name is anything other than
    Macintosh HD and resets it.

---
+++ HDBootVolName.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Script to find the name of the current boot volume
+
+# Author  : r.purves@arts.ac.uk
+# Version : 1.0 - Initial Version
+# Version : 1.0 - Changed from reporting only to fixing
+
+# Grab the current boot volume name (sanity check just in case smart group is inaccurate)
+
+BootVolume=$( diskutil info / | grep "Volume Name" | cut -c 30- )
+
+# Check and rename if necessary
+
+if [ "$VolumeName" != "Macintosh HD" ];
+then
+	diskutil renameVolume "$BootVolume" "Macintosh HD"
+fi
+
+# Grab the current boot volume name again and report to the JSS.
+
+BootVolume=$( diskutil info / | grep "Volume Name" | cut -c 30- )
+
+echo "<result>$BootVolume</result>"
+
+exit 0
