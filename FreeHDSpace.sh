d31e9955cc456ecec90a46370ee98c6d7d592d23
Richard Purves <franton@users.noreply.github.com>
2014-09-12 23:22:00 +0100

    Update FreeHDSpace.sh

--- FreeHDSpace.sh
+++ FreeHDSpace.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Script to report the utilised space on the / mount point
-# Based on work by j.durler@arts.ac.uk
 
-# Author      : r.purves@arts.ac.uk
+# Author      : contact@richard-purves.com
 # Version 1.0 : 1-11-2013 - Initial Version
 
 echo "<result>`df / | grep "/" | awk '{print $8}'`</result>"
