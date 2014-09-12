266f7e1326033a7d055d10f1fc026322a3dbdd75
Richard Purves <franton@users.noreply.github.com>
2014-09-12 23:26:10 +0100

    Update LastStartupTime.sh

--- LastStartupTime.sh
+++ LastStartupTime.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script to interrogate computer on the last time it started up
-# Author:  r.purves@arts.ac.uk
+# Author:  contact@richard-purves.com
 # Version 1.0 : 9-1-2013 - Initial Version
 
 time=$( sysctl kern.boottime | cut -c46- )
