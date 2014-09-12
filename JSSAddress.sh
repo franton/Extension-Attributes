627173270f61fd8d1b7f5d49682e5cd166dde129
Richard Purves <franton@users.noreply.github.com>
2014-09-12 23:23:57 +0100

    Update JSSAddress.sh

--- JSSAddress.sh
+++ JSSAddress.sh
@@ -2,7 +2,7 @@
 
 # Script to determine the JSS address on a target mac
 
-# Author : r.purves@arts.ac.uk
+# Author : contact@richard-purves.com
 # Version 1.0 : 21-2-2013 - Initial Version.
 
 result=$( defaults read /Library/Preferences/com.jamfsoftware.jamf.plist jss_url )
