f8678dc677e74cfb25c60287fc8fca24f5c064a1
Richard Purves <franton@users.noreply.github.com>
2014-09-12 23:25:42 +0100

    Update LastImagedTime.sh

--- LastImagedTime.sh
+++ LastImagedTime.sh
@@ -3,7 +3,7 @@
 # Script to report when the computer was last imaged
 # This is reliant on a timestamp file created as part of the firstrun script.
 
-# Author	 	: r.purves@arts.ac.uk
+# Author	 	: contact@richard-purves.com
 # Version 1.0	: 09-08-2013 - Initial Version
 
 if [ -z /usr/lastimaged ] ;
