dee187eccc75f1a5a16ed17567c00af3b0e86d18
Richard Purves <franton@users.noreply.github.com>
2014-09-12 23:24:55 +0100

    Update LastAdminPWChange.sh

--- LastAdminPWChange.sh
+++ LastAdminPWChange.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
 # Script to report the last time the local account had it's password changed.
-# This should be the same day as the computer was powered on.
 
-# Author : r.purves@arts.ac.uk
+# Author : contact@richard-purves.com
 # Version: 1.0 - Initial Version
 
-# In order this will read the uadmin account record
+# In order this will read the admin account record
 # grep the last password set time field plus the line after it.
 # delete the first line to remove the xml header field
 # remove all spaces
 # strip off the <date> and </date> tags
 # replace the T and Z characters with spaces to make things readable.
 
-lastdate=$( dscl . read /Users/uadmin | grep -A1 passwordLastSetTime | sed '1d' | sed 's/ $//g' | sed -e "s/<date>\(.*\)<\/date>/\1/" | tr "|" " " | sed 's/T/\ /' | sed 's/Z/\ /' )
+lastdate=$( dscl . read /Users/admin | grep -A1 passwordLastSetTime | sed '1d' | sed 's/ $//g' | sed -e "s/<date>\(.*\)<\/date>/\1/" | tr "|" " " | sed 's/T/\ /' | sed 's/Z/\ /' )
 
 # Now report to the JSS
 
