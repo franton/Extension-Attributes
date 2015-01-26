d90a6e0ee3e44b0e28a9f00b7ec96c5443d346ea
Richard Purves <franton@users.noreply.github.com>
2015-01-26 17:00:04 +0000

    Update LastStartupTime.sh

--- LastStartupTime.sh
+++ LastStartupTime.sh
@@ -3,7 +3,11 @@
 # Script to interrogate computer on the last time it started up
 # Author:  contact@richard-purves.com
 # Version 1.0 : 9-1-2013 - Initial Version
+# Version 2.0 : 26-1-2015 - Ripped off mm2270 on https://jamfnation.jamfsoftware.com/discussion.html?id=13122
+# This variant is superior to my version as it allows use of the JSS EA date reporting rather than text field.
+# Now you can far more easily use smart groups to keep track of this. Thanks Mikey!
 
-time=$( sysctl kern.boottime | cut -c46- )
+lastBootRaw=$(sysctl kern.boottime | awk '{print $5}' | sed 's/,$//')
+lastBootFormat=$(date -jf "%s" "$lastBootRaw" +"%Y-%m-%d %T")
 
-echo "<result>${time}</result>"
+echo "<result>$lastBootFormat</result>"
