18d6a2d4c3914906fb557b9dad28e47e9162c290
Richard Purves <franton@users.noreply.github.com>
2016-05-15 09:43:22 +0100

    Create RecoveryPartitionVersion.sh

---
+++ RecoveryPartitionVersion.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Report current Recovery Partition version
+
+recoverypart=$( diskutil list | grep "Recovery HD" | awk '{ print $7 }' )
+
+mkdir /Volumes/Recovery\ HD
+mount -t hfs -o nobrowse /dev/$recoverypart /Volumes/Recovery\ HD
+
+version=$( defaults read /Volumes/Recovery\ HD/System/Library/CoreServices/SystemVersion.plist ProductVersion )
+
+diskutil unmount $recoverypart
+
+echo "<result>$version</result>"
