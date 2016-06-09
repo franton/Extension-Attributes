461395c97ef035c08ffce7150d5f5a48f767392c
Richard Purves <franton@users.noreply.github.com>
2016-06-09 10:41:05 +0200

    Update ftpservice.sh

--- ftpservice.sh
+++ ftpservice.sh
@@ -4,7 +4,7 @@
 
 check=$( launchctl list | grep ftp | awk '{ print $3 }' )
 
-if [ $check = "com.apple.servermgrd.ftp" ];
+if [ "$check" = "com.apple.servermgrd.ftp" ];
 then
 	echo "<result>Enabled</false>"
 else
