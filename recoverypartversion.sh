f592299c9ef5e17b89861bce3a1c2cc6227bac5e
Richard Purves <franton@users.noreply.github.com>
2016-06-09 10:39:54 +0200

    Update recoverypartversion.sh

--- recoverypartversion.sh
+++ recoverypartversion.sh
@@ -5,10 +5,10 @@
 recoverypart=$( diskutil list | grep "Recovery HD" | awk '{ print $7 }' )
 
 mkdir /Volumes/Recovery\ HD
-mount -t hfs -o nobrowse /dev/$recoverypart /Volumes/Recovery\ HD
+mount -t hfs -o nobrowse /dev/$recoverypart /Volumes/Recovery\ HD 2>/dev/null
 
 version=$( defaults read /Volumes/Recovery\ HD/System/Library/CoreServices/SystemVersion.plist ProductVersion )
 
-diskutil unmount $recoverypart
+diskutil unmount $recoverypart > /dev/null
 
 echo "<result>$version</result>"
