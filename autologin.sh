e19466c616279979e830ca9b5d783fe95b10c522
Richard Purves <franton@users.noreply.github.com>
2016-07-14 20:04:36 +0200

    Update autologin.sh

--- autologin.sh
+++ autologin.sh
@@ -2,7 +2,7 @@
 
 # EA to check for Automatic Logins
 
-check=$( defaults read /Library/Preferences/com.apple.loginwindow.plist | grep autoLoginUser' )
+check=$( defaults read /Library/Preferences/com.apple.loginwindow.plist | grep autoLoginUser )
 
 if [ "$check" != "" ];
 then
