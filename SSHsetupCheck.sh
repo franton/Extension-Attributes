b7bc6f4866d78c3c9582dc661bc7c713e5f0ffde
Richard Purves <franton@users.noreply.github.com>
2016-12-23 21:29:03 +0000

    Update SSHsetupCheck.sh
    Neater way of doing things

--- SSHsetupCheck.sh
+++ SSHsetupCheck.sh
@@ -2,13 +2,6 @@
 
 # Script to determine if the ssh script has been run or not.
 
-test=$(dseditgroup -o read -t group com.apple.access_ssh | grep dsAttrTypeStandard:NestedGroups -A 1 | grep 03E0A4CA-C915-412C-9F1E-536DA4DB4C93 | wc -l | awk '{print $1}')
+test=$( dseditgroup -o read -t group com.apple.access_ssh | awk '/dsAttrTypeStandard:GroupMembership/{y=1;next}y' | xargs )
 
-if [[ test -eq 0 ]]
-then
-   echo "<result>Incorrect</result>"
-else
-   echo "<result>Correct</result>"
-fi
-
-exit 0
+echo "<result>$test</result>"
